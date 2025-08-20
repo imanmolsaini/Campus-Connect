196fcec7c2cd835c895bc78a82c404f3da6fcee8
anmolsaini2002 <anmolsaini2002@gmail.com>
2025-08-20 22:40:47 +1200

    fixed seeds.sql path inside002_seed_data.sql

--- database/migrations/002_seed_data.sql
+++ database/migrations/002_seed_data.sql
@@ -2,7 +2,7 @@
 -- Created: 2024-01-07
 -- Description: Insert initial test data
 
-\i seeds.sql
+\i database/seeds.sql
 
 -- Record this migration
 INSERT INTO migrations (migration_name) VALUES ('002_seed_data');
