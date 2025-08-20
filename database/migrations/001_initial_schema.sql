0a794831a98fe0ddfde56ab19b3e2c8efaf6cc4a
anmolsaini2002 <anmolsaini2002@gmail.com>
2025-08-20 22:00:36 +1200

    001_initial_schema.sql create database for project

---
+++ database/migrations/001_initial_schema.sql
@@ -0,0 +1,17 @@
+-- Migration 001: Initial Schema Creation
+-- Created: 2024-01-07
+-- Description: Create initial database schema for Campus Connect NZ
+
+-- This migration creates the initial database structure
+-- Run this file to set up the database from scratch
+
+\i schema.sql
+
+-- Record this migration
+CREATE TABLE IF NOT EXISTS migrations (
+    id SERIAL PRIMARY KEY,
+    migration_name VARCHAR(255) NOT NULL,
+    executed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO migrations (migration_name) VALUES ('001_initial_schema');
