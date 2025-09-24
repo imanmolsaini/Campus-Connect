57bebfc717e79b59da96dd35e13f8f537c6ee88c
Grregorry <gradysfield@gmail.com>
2025-09-24 12:12:45 +1200

    change club placeholder name

--- frontend/src/app/clubs/page.tsx
+++ frontend/src/app/clubs/page.tsx
@@ -219,7 +219,7 @@
             <div className="relative">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
               <Input
-                placeholder="Search clubs..."
+                placeholder="Search club name..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 className="pl-10"
